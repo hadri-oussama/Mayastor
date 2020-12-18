562a833c43c88577d734101f8c43709ca4b66391
Blaise Dias <blaise.dias@mayadata.io>
2020-12-18 15:35:55 +0000

    Run CSI e2e tests in CI/CD pipeline CAS-577
    Refactor scripts/e2e-test.sh to
     * Run the CSI tests
     * Be more developer friendly
    
    1. Add csi to the set of tests run by default
    2. The set of tests to be run can now be specified
       using env var e2e_tests
    3. Run uninstall test only if specified,
        and always after all other tests have
        run successfully or not.

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -2,9 +2,26 @@
 
 set -eux
 
-SCRIPTDIR=$(dirname "$(realpath "$0")")
+SCRIPTSDIR=$(dirname "$(realpath "$0")")
+TESTDIR=$(realpath "$SCRIPTSDIR/../test/e2e")
 REGISTRY=$1
-TESTS="install basic_volume_io"
+TESTS=${e2e_tests:-"install basic_volume_io csi uninstall"}
+
+# Run go test in directory specified as $1 (relative path)
+function runGoTest {
+    echo "Run go test in $PWD/\"$1\""
+    if [ -z "$1" ] || [ ! -d "$1" ]; then
+        return 1
+    fi
+
+    cd "$1"
+    if ! go test -v . -ginkgo.v -ginkgo.progress -timeout 0; then
+        return 1
+    fi
+
+    return 0
+}
+
 
 # TODO: Add proper argument parser
 if [ -z "$REGISTRY" ]; then
@@ -14,23 +31,50 @@ fi
 
 test_failed=
 export e2e_docker_registry="$REGISTRY"
-export e2e_pool_device=/dev/nvme1n1
+export e2e_pool_device=${e2e_pool_device:-/dev/nvme1n1}
 
 for dir in $TESTS; do
-  cd "$SCRIPTDIR/../test/e2e/$dir"
-  if ! go test -v . -ginkgo.v -ginkgo.progress -timeout 0 ; then
-    test_failed=1
-    break
-  fi
+  cd "$TESTDIR"
+  case "$dir" in
+      uninstall)
+          # defer to next loop
+          ;;
+      csi)
+        # TODO move the csi-e2e tests to a subdirectory under e2e
+        # issues with using the same go.mod file prevents this at the moment.
+        cd "../csi-e2e/"
+        if ! ./runtest.sh ; then
+            test_failed=1
+            break
+        fi
+        ;;
+      *)
+        if ! runGoTest "$dir" ; then
+            test_failed=1
+            break
+        fi
+        ;;
+  esac
 done
 
-# must always run uninstall test in order to clean up the cluster
-cd "$SCRIPTDIR/../test/e2e/uninstall"
-go test
+for dir in $TESTS; do
+  cd "$TESTDIR"
+  case "$dir" in
+      uninstall)
+        echo "Uninstalling mayastor....."
+        if ! runGoTest "$dir" ; then
+            test_failed=1
+        fi
+        ;;
+      *)
+        ;;
+   esac
+done
 
 if [ -n "$test_failed" ]; then
+    echo "At least one test has FAILED!"
   exit 1
 fi
 
-echo "All tests have passed"
+echo "All tests have PASSED!"
 exit 0
